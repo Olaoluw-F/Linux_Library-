650a7d707a339d11582c9b2bf3af40558e644d25
Olaoluwa.F <118767486+Olaoluw-F@users.noreply.github.com>
2026-03-23 01:43:22 -0400

    Add files via upload

---
+++ web_service.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+nginx_s=$(nginx -v)
+webserver='nginx'
+echo "--- Current Nginx Version ---"
+$webserver
+echo "------ MENU SCREEN ------"
+echo "Menu for web service command 1: stop, 2: start, 3: restart, or 4: status"
+read -p "Choose what option of the menu you want to complete: " option
+
+case $option in
+	1)
+		sudo systemctl stop $webserver
+		echo "--- Webserver Stopped ---"
+		;;
+	
+	2)
+		sudo systemctl start $webserver
+		echo "--- Webserver Started ---"
+		;;
+
+	3)
+		sudo systemctl restart $webserver
+		echo "--- Webserver Restarted ---"
+		;;
+
+	4)
+		sudo systemctl status $webserver
+		;;
+esac 
+
